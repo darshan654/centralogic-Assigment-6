c5959e7f34f7afae21d8214a3baeb89820e780d3
darshan <sudthakare73@gmail.com>
2024-06-20 01:16:46 +0530

    did some change

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-// App.js
 import React, { useEffect, useState } from 'react';
 import CurrencyRow from './CurrencyRow';
-import './App.css'
+import './App.css';
 
 function App() {
   const Base_Url = "https://api.exchangerate-api.com/v4/latest/INR";
   const [currencyOptions, setCurrencyOptions] = useState([]);
 
-
-
   useEffect(() => {
     fetch(Base_Url)
       .then(res => res.json())
       .then(jsonData => {
-        setCurrencyOptions([jsonData.base, jsonData.rates, ...Object.keys(jsonData.rates),]);
-
-
+        setCurrencyOptions([jsonData.base, jsonData.rates, ...Object.keys(jsonData.rates)]);
         console.log(jsonData);
       })
       .catch(error => console.log("Something went wrong:", error));
   }, []);
 
   return (
-    <div style={{ backgroundColor: "white", margin: "50px", border: "3px solid black ", padding: "10px", borderRadius: "14px" }}>
-
-      <h1 style={{ fontSize: "50px" }}> Currency Converter</h1>
-      <CurrencyRow name="To" currencyOptions={currencyOptions} />
+    <div className="bg-white m-12 p-6 border-2 border-black rounded-lg shadow-lg">
+      <h1 className="text-4xl font-bold mb-4 text-center">Currency Converter</h1>
+      <CurrencyRow name=" INDIA amount" currencyOptions={currencyOptions} />
     </div>
-
   );
 }
 
--- src/CurrencyRow.jsx
+++ src/CurrencyRow.jsx
@@ -1,11 +1,9 @@
-// CurrencyRow.js
 import React, { useState } from 'react';
 
 function CurrencyRow(props) {
   const [myValue, setMyValue] = useState('');
   const { name, currencyOptions } = props;
   const [selectedOptionKey, setSelectedOption] = useState('');
-
 
   const base = currencyOptions ? currencyOptions[0] : '';
   const rates = currencyOptions ? currencyOptions[1] : '';
@@ -24,20 +22,28 @@ function CurrencyRow(props) {
       <label htmlFor="from" className="block font-semibold mb-2">
         {name}
       </label>
-      <input type='number' id='from' value={myValue} onChange={handleInputChange} className="border border-black rounded-md px-3 py-2 mb-2" />
-      <select onChange={handleSelectChange} className="border  border-black rounded-md px-3 py-2">
-        {options.map((cname) =>
+      <input
+        type='number'
+        id='from'
+        value={myValue}
+        onChange={handleInputChange}
+        className="border border-black rounded-md px-3 py-2 mb-2 w-full"
+      />
+      <select
+        onChange={handleSelectChange}
+        className="border border-black rounded-md px-3 py-2 w-full"
+      >
+        {options.map((cname) => (
           <option key={cname} value={cname}>{cname}</option>
-        )}
+        ))}
       </select>
       <br /><br />
-
       <h1 className="font-semibold">Selected Option: {selectedOptionKey}</h1>
       {rates && selectedOptionKey ? (
         <h1 className="font-semibold">{myValue * rates[selectedOptionKey]}</h1>
-      ) :
+      ) : (
         <h1>0</h1>
-      }
+      )}
     </div>
   );
 };
